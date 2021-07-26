927a5bd2326737d636e1c80c936f474ca566db32
Vinicius de Santana <vinicius.vts@gmail.com>
2021-07-26 16:45:42 -0300

    fix: enable count when page is acessed

--- wp-content/themes/dna/single.php
+++ wp-content/themes/dna/single.php
@@ -1,6 +1,6 @@
 <?php
 // Adiciono um view ao post
-// ssw_addViewToPost(get_the_ID());
+ssw_addViewToPost(get_the_ID());
 get_header();
 get_template_part('template-parts/header', 'blog-single');
 get_template_part('template-parts/content', 'blog-single');
