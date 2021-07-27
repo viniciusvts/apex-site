0fbb33ad523f3b65ef384c1bc32284b95ff42284
Vinicius de Santana <vinicius.vts@gmail.com>
2021-07-27 11:14:57 -0300

    feat: page

---
+++ wp-content/themes/dna/template-parts/content-page.php
@@ -0,0 +1,19 @@
+<section class="blog-single container">
+    <div class="row">
+        <div class="col-lg-10 col-xxl-8 mx-auto">
+            </div>
+            <div class="row crumbs">
+                <div class="col-12 mb-3 mb-lg-5">
+                <?php
+                wp_custom_breadcrumbs();
+                ?>
+                </div>
+            </div>
+            <div class="row">
+                <article class="col-12">
+                    <?php the_content(); ?>
+                </article>
+            </div>
+        </div>
+    </div>
+</section>
