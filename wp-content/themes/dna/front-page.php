1c3625c09b1e997f971cd75bab318d8f99b1a591
Vinicius de Santana <vinicius.vts@gmail.com>
2021-07-27 11:26:50 -0300

    feat: add section "milhares de sonhos"

--- wp-content/themes/dna/front-page.php
+++ wp-content/themes/dna/front-page.php
@@ -2,7 +2,7 @@
 <?php
 get_header();
 get_template_part('template-parts/header', 'home');
-get_template_part('template-parts/section', 'saidoaluguel');
+get_template_part('template-parts/section', 'milharessonhos');
 get_template_part('template-parts/imoveis', 'slide');
 get_template_part('template-parts/blog', 'feed');
 get_template_part('template-parts/form', 'newsletter');
---
+++ wp-content/themes/dna/template-parts/section-milharessonhos.php
@@ -0,0 +1,42 @@
+<section class="container saidoaluguel">
+    <div class="row">
+        <div class="col-12 d-flex">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/casa-verde-e-amarela.jpg"
+            alt="logo casa verde e amarela"
+            class="mx-auto mb-3 d-lg-none">
+        </div>
+        <div class="col-12 col-lg-6">
+            <h2 class="text-center bold italic d-lg-none">
+                MILHARES DE FAMÍLIAS JÁ REALIZARAM SEUS SONHOS
+            </h2>
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/milhares-sonhos.jpg"
+            alt="familia feliz com a apex"
+            class="w-100 mb-5">
+        </div>
+        <div class="col-12 col-lg-6">
+            <h2 class="bold italic d-none d-lg-block mt-5">
+                MILHARES DE FAMÍLIAS JÁ REALIZARAM SEUS SONHOS
+            </h2>
+            <p>A Apex  Engenharia cresceu e se desenvolveu junto com o desejo
+                dos habitantes da Capital de fazer de Brasília uma cidade perfeita
+                para morar.
+            </p>
+            <p>A empresa atua no desenvolvimento do mercado voltado para a habitação, 
+                oferecendo à cidade empreendimentos práticos, funcionais e sustentáveis. 
+                Já entregou mais de 100 empreendimentos.
+            </p>
+            <p class="mb-5">Satisfazer o cliente é a principal meta da Apex, cujos 
+                trabalhos partem da realização de estudos e rigoroso planejamento 
+                que atendam às principais necessidades do público, até  a 
+                concretização do sonho da casa própria.
+            </p>
+            <div class="row">
+                <div class="col d-flex">
+                    <a href="#" class="btn btn-b mx-auto mt-4">
+                        Fale com um consultor
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
