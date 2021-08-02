288db11d365e035dda8bc307416cdcdb37567480
Vinicius de Santana <vinicius.vts@gmail.com>
2021-08-02 13:25:00 -0300

    fix: animate modal-buscar

--- wp-content/themes/dna/template-parts/modal-buscar.php
+++ wp-content/themes/dna/template-parts/modal-buscar.php
@@ -1,5 +1,5 @@
 <!-- No html utilize data-target="modal-buscar" -->
-<section class="modal-buscar" id=modal-buscar>
+<section class="ssw-modal modal-buscar" id=modal-buscar>
     <div class="intern">
         <?php get_search_form(); ?>
     </div>
