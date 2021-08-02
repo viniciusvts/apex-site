4fb80bb7e612181fff4bca5f66bac8d429d44e1a
Vinicius de Santana <vinicius.vts@gmail.com>
2021-08-02 12:36:51 -0300

    fix: animate modal-simulador

--- wp-content/themes/dna/template-parts/section-simulador-modal.php
+++ wp-content/themes/dna/template-parts/section-simulador-modal.php
@@ -1,6 +1,6 @@
 <!-- MODAL para exibir o simulador
 No tag img utilize data-modalSimula -->
-<section class="modal-simulador" id=modal-simulador>
+<section class="ssw-modal modal-simulador" id=modal-simulador>
     <div class="intern simulador">
         <?php include get_template_directory().'/template-parts/sub_section/simulador.php'; ?>
 </div>
