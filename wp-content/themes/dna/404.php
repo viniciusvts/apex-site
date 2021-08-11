d9693fd20f44006a8873e141f7b3fb46a290f5a1
Vinicius de Santana <vinicius.vts@gmail.com>
2021-08-11 16:18:05 -0300

    fix: layout of 404 page

--- wp-content/themes/dna/404.php
+++ wp-content/themes/dna/404.php
@@ -1,19 +1,31 @@
 <?php
 get_header();
+?><?php
+$pageId = get_option( 'page_for_posts' );
+$thumb = get_the_post_thumbnail($pageId,
+                    'post-thumbnail',
+                    array( 'class' => 'fundo w-100 h-100' )
+);
 ?>
-<header class="pageContent">
-    <?php echo(file_get_contents(get_template_directory() . '/assets/img/headerArt.svg')); ?>
+<header class="header-title-sub">
+    <?php echo $thumb; ?>
     <div class="container">
-        <div class="row no-gutters">
-            <h1>Página não encontrada</h1>
-        </div>
-        <div class="row no-gutters">
-            <h2>Olá, você tentou acessar uma página que não existe,
-            ainda assim podemos te ajudar, entre em contato conosco e nos mande uma mensagem
-            teremos prazer em lhe responder</h2>
+        <div class="row content">
+            <div class="col-xl-4 title">
+                <h1>Página não encontrada</h1>
+            </div>
         </div>
     </div>
 </header>
+<section class="pageContent">
+    <div class="container my-5 py-5">
+        <div class="row no-gutters">
+            <p>Olá, você tentou acessar uma página que não existe,
+            ainda assim podemos te ajudar, entre em contato conosco e nos mande uma mensagem
+            teremos prazer em lhe responder</p>
+        </div>
+    </div>
+</section>
 <?php
 get_template_part('template-parts/section', 'sabemosOQQuer');
 get_template_part('template-parts/section', 'sabemosOQVcPrecisa');
