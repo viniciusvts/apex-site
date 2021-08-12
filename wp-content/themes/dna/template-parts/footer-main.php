950fc390dfcae865fbfc703b87b4348f43b218bc
Vinicius de Santana <vinicius.vts@gmail.com>
2021-08-12 16:02:26 -0300

    fix: change text of contacts
    Client requests to change address
    Motive: in BR is usual to write "city/State"
    Realized: change address to city/state

--- wp-content/themes/dna/template-parts/footer-main.php
+++ wp-content/themes/dna/template-parts/footer-main.php
@@ -33,12 +33,18 @@
                 <h2>Contato</h2>
                 <h3>ÁREA ADM. E COMERCIAL</h3>
                 <p class="tel"><a href="tel:+55 61 3361 4455">+55 61 3361 4455</a></p>
-                <address>Sia Trecho 6, Lote 90/100 <br>
-                    Brasília – DF – CEP 71205-060</address>
+                <address>
+                    Sia Trecho 6, Lote 90/100 <br>
+                    Brasília/DF <br>
+                    CEP 71205-060
+                </address>
                 <h3 class="mt-4">ÁREA DE VENDAS</h3>
                 <p class="tel"><a href="tel:+55 61 3905 3090">+55 61 3905 3090</a></p>
-                <address>QS 406, Conjunto C, Loja 4 <br>
-                Samambaia – DF – CEP 72318-573</address>
+                <address>
+                    QS 406, Conjunto C, Loja 4 <br>
+                    Samambaia/DF <br>
+                    CEP 72318-573
+                </address>
                 <!-- redes sociais -->
                 <div class="row">
                     <div class="col-2">
