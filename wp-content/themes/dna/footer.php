ce39e745b48d41558e644eff88dcc1a6e4528f44
Vinicius de Santana <vinicius.vts@gmail.com>
2021-08-13 11:27:32 -0300

    fix: change position of popups to footer

--- wp-content/themes/dna/footer.php
+++ wp-content/themes/dna/footer.php
@@ -1,4 +1,10 @@
         <?php
+        get_template_part('template-parts/float', 'contato');
+        get_template_part('template-parts/section', 'simulador-modal');
+        get_template_part('template-parts/rd', 'pop-whats');
+        get_template_part('template-parts/rd', 'pop-email');
+        get_template_part('template-parts/rd', 'pop-attonline');
+        get_template_part('template-parts/rd', 'pop-client');
         get_template_part('template-parts/footer', 'main');
         wp_footer();
         ?>
--- wp-content/themes/dna/header.php
+++ wp-content/themes/dna/header.php
@@ -10,11 +10,5 @@
     <?php
     get_template_part('template-parts/' . 'menu');
     get_template_part('template-parts/modal', 'buscar');
-    get_template_part('template-parts/float', 'contato');
-    get_template_part('template-parts/section', 'simulador-modal');
-    get_template_part('template-parts/rd', 'pop-whats');
-    get_template_part('template-parts/rd', 'pop-email');
-    get_template_part('template-parts/rd', 'pop-attonline');
-    get_template_part('template-parts/rd', 'pop-client');
     ?>
     
